47f0885754a4675fe8337c9db10415921fd17dab
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-04-17 14:05:16 +0200

    add env file for credentials

--- data/db.js
+++ data/db.js
@@ -1,10 +1,10 @@
 const mysql=require("mysql2")
 
 const credentials={
-    host:"localhost",
-    user:"root",
-    password:"password",
-    database:"blog"
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME
 }
 
 const connection = mysql.createConnection(credentials)
