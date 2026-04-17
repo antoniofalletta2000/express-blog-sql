2953c156727717045f6af815f77ee913223899e9
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-04-17 14:00:08 +0200

    Milestone 4

--- controllers/postController.js
+++ controllers/postController.js
@@ -15,12 +15,25 @@ const index= (req, res) => {
 }
 
 const show=(req,res)=>{
-    const post= posts.find(post=>post.id===parseInt(req.params.id))
-    if(post){
-        res.json(post)
-    }else{
-        res.status(404).json({message:"Post not found"})
-    }
+    
+    const {id}=req.params
+
+    const sql= "SELECT * FROM posts WHERE id=?"
+
+    connection.query(sql, [id], (err, results)=>{
+
+        if(err) return res.status(500).json({
+            error:"Database query failed"
+        })
+        
+        if(results.length===0) return res.status(404).json({
+            error:true,
+            message:"Posts not found"
+        })
+        
+        res.json(results[0])
+    })
+    
 }
 
 const store=(req,res)=>{
